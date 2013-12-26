d85e2ff1ab7b51d94bf5153f0b6b884591473e96
Alvaro Muir <alvaromuir@gmail.com>
2013-12-26 00:37:17 -0500

    We can not delete categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -39,6 +39,15 @@ class CategoriesController < ApplicationController
       render "edit"
     end
   end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    flash[:notice] = "Category has been deleted."
+
+    redirect_to categories_path
+  end
   
   private
 
