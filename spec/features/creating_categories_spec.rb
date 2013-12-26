f17a61da649a4f286c167593a3c1e7875385b463
Alvaro Muir <alvaromuir@gmail.com>
2013-12-25 23:30:34 -0500

    Dynamic page titles

--- spec/features/creating_categories_spec.rb
+++ spec/features/creating_categories_spec.rb
@@ -6,10 +6,17 @@ feature 'Creating Categories' do
     visit '/'
     click_link 'New Category'
 
-    fill_in 'Name', with: 'HTML5  CSS3'
+    fill_in 'Name', with: 'HTML5 CSS3'
     fill_in 'Description', with: 'All things browser markup'
     click_button 'Create Category'
 
     expect(page).to have_content('Category has been created.')
+
+    category = Category.where(name: "HTML5 CSS3").first
+
+    expect(page.current_url).to eql(category_url(category))
+
+    title = "Categories | HTML5 CSS3 => Blackstar"
+    expect(page).to have_title(title)
   end
 end
