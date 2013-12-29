11543681eb6ed9612081473c84d9fb6b07f9ceec
Alvaro Muir <alvaromuir@gmail.com>
2013-12-28 22:56:32 -0500

    restrict actions on TopicsController based on permission to hide links

--- Gemfile
+++ Gemfile
@@ -54,4 +54,5 @@ group :test do
     gem 'capybara', "2.1.0"
     gem 'factory_girl_rails', '~> 4.2.1'
     gem 'cancan', '~> 1.6.10'
+    gem 'launchy'
 end
--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -5,6 +5,9 @@ feature "hidden links" do
   let(:user) { FactoryGirl.create(:user) }
   let(:admin) { FactoryGirl.create(:admin_user) }
   let(:category) { FactoryGirl.create(:category) }
+  let(:topic) { FactoryGirl.create(:topic,
+                                    category: category,
+                                    user: user) }
 
   context "anonymous users" do
     scenario "cannot see the New Category link" do
@@ -25,6 +28,7 @@ feature "hidden links" do
 
   context "regular users" do
     before { sign_in_as!(user) }
+    
     scenario "cannot see the New Category link" do
       visit '/'
       assert_no_link_for "New Category"
@@ -38,6 +42,53 @@ feature "hidden links" do
     scenario "cannot see the Delete Category link" do
       visit category_path(category)
       assert_no_link_for "Delete Category"
+    end
+
+    scenario "New topic link is shown to a user with persmission" do
+      define_permission!(user, "view", category)
+      define_permission!(user, "create topics", category)
+      visit category_path(category)
+      assert_link_for "New Topic"
+    end
+
+    scenario "New topic link hidden from a user with persmission" do
+      define_permission!(user, "view", category)
+      visit category_path(category)
+      assert_no_link_for "New Topic"
+    end
+
+    scenario "Edit topic link is shown to a user with permission" do
+      topic
+      define_permission!(user, "view", category)
+      define_permission!(user, "edit topics", category)
+      visit category_path(category)
+      click_link topic.title
+      assert_link_for "Edit Topic"
+    end
+
+    scenario "Edit topic link is hidden from a user without permission" do
+      topic
+      define_permission!(user, "view", category)
+      visit category_path(category)
+      click_link topic.title
+      assert_no_link_for "Edit Topic"
+    end
+
+    scenario "Delete topic link is shown to a user with permission" do
+      topic
+      define_permission!(user, "view", category)
+      define_permission!(user, "delete topics", category)
+      visit category_path(category)
+      click_link topic.title
+      assert_link_for "Delete Topic"
+    end
+
+    scenario "Delete topic link is hidden from a user without permission" do
+      topic
+      define_permission!(user, "view", category)
+      visit category_path(category)
+      click_link topic.title
+      assert_no_link_for "Delete Topic"
     end
   end
 
@@ -56,6 +107,25 @@ feature "hidden links" do
     scenario "can see the Delete Category link" do
       visit category_path(category)
       assert_link_for "Delete Category"
+    end
+
+    scenario "New topic link is shown to admins" do
+      visit category_path(category)
+      assert_link_for "New Topic"
+    end
+
+    scenario "Edit topic link is shown to admins" do
+      topic
+      visit category_path(category)
+      click_link topic.title
+      assert_link_for "Edit Topic"
+    end
+
+    scenario "Delete topic link is shown to admins" do
+      topic
+      visit category_path(category)
+      click_link topic.title
+      assert_link_for "Delete Topic"
     end
   end 
 end
