20bb9b35af5c734f4f1a9b415d654dd79929c534
Alvaro Muir <alvaromuir@gmail.com>
2013-12-29 13:16:03 -0500

    Add creating, editing, updating and deleting topics to asigning permissions interface

--- app/helpers/admin/permissions_helper.rb
+++ app/helpers/admin/permissions_helper.rb
@@ -1,7 +1,10 @@
 module Admin::PermissionsHelper
   def permissions
     {
-      "view" => "View"
+      "view" => "View",
+      "create topics" => "Create Topics",
+      "edit topics" => "Edit Topics",
+      "delete topics" => "Delete Topics"
     } 
   end
 end
--- spec/features/admin/assigning_permissions_spec.rb
+++ spec/features/admin/assigning_permissions_spec.rb
@@ -29,4 +29,49 @@ feature "Assinging permissions" do
     sign_in_as!(user)
     expect(page).to have_content(category.name)
   end
+
+  scenario "Creating topics for a category" do
+    check_permission_box "view", category
+    check_permission_box "create_topics", category
+    click_button "Update"
+    click_link "Sign out"
+    sign_in_as!(user)
+    click_link category.name
+    click_link "New Topic"
+    fill_in "Title", with: "HTML5!"
+    fill_in "Description", with: "Browser support?"
+    click_button "Create"
+    expect(page).to have_content("Topic has been created.")
+  end
+
+  scenario "Updating a topic for a category" do
+    check_permission_box "view", category
+    check_permission_box "edit_topics", category
+    click_button "Update"
+    click_link "Sign out"
+
+    sign_in_as!(user)
+    click_link category.name
+    click_link topic.title
+    click_link "Edit Topic"
+    fill_in "Title", with: "Mobile browser suport?"
+    click_button "Update Topic"
+
+    expect(page).to have_content("Topic has been updated")
+  end
+
+  scenario "Deleting a topic for a category" do
+    check_permission_box "view", category
+    check_permission_box "delete_topics", category
+
+    click_button "Update"
+    click_link "Sign out"
+
+    sign_in_as!(user)
+    click_link category.name
+    click_link topic.title
+    click_link "Delete Topic"
+
+    expect(page).to have_content("Topic has been deleted.")
+  end
 end
